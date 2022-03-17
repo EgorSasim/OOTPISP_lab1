a3b836b57f164c274b81b336d68e19a2facec067
Gorgy <egorsasim@mail.ru>
2022-03-17 17:59:51 +0300

    use wpf to draw all shapes

--- ShapesMaker/MainWindow.xaml.cs
+++ ShapesMaker/MainWindow.xaml.cs
@@ -24,23 +24,107 @@ namespace ShapesMaker
         {
             InitializeComponent();
 
+            Line line = new Line(new Point(100,200), new Point(700,200));
+            Rectangle rectangle = new Rectangle(new Point(400, 50), new Point(400, 0), new Point(500, 0), new Point
+                (500, 50));
+            Square square = new Square(new Point(300, 50), new Point(300, 0), new Point(350, 0), new Point
+                (350, 50));
+            Circle circle = new Circle(new Point(200, 50), 50);
+            Triangle triangle = new Triangle(new Point(600, 100), new Point(650, 50), new Point(700, 100));
+            Elipse elipse = new Elipse(new Point(50, 50), 100, 50);
 
-            Shape line = new Line(new Point(1,1), new Point(2,2));
-            Shape rectangle = new Rectangle(new Point(1,1), new Point(1,5), new Point(2,5), new Point
-                (2,1));
-            Shape square = new Square(new Point(1, 1), new Point(1, 3), new Point(3, 3), new Point
-                (3, 1));
-            Shape circle = new Circle(new Point(1, 1), 45);
-            Shape triangle = new Triangle(new Point(1, 1), new Point(1, 3), new Point(3, 3));
-            Shape elipse = new Elipse(new Point(1, 1), 155, 45);
-
-
-            MessageBox.Show(line.Draw());
-            MessageBox.Show(rectangle.Draw()); 
-            MessageBox.Show(square.Draw());
-            MessageBox.Show(circle.Draw());
-            MessageBox.Show(triangle.Draw());
+            // Message boxes to know the cirtain coordinates of my shapes
             MessageBox.Show(elipse.Draw());
+            MessageBox.Show(circle.Draw());
+            MessageBox.Show(square.Draw());
+            MessageBox.Show(rectangle.Draw());
+            MessageBox.Show(triangle.Draw());
+            MessageBox.Show(line.Draw());
+
+            // Create a canvas because Window.AddChild will throw exception when U add more than one 
+            // object to it, because Window is a ContentControl. Page allowed only 1 chil, so this 
+            // child will be a canv and I will place all other objects in canv;
+            Canvas canv = new Canvas();
+
+            //Line
+            System.Windows.Shapes.Line lin = new System.Windows.Shapes.Line();
+            lin.X1 = line.P1.X;
+            lin.Y1 = line.P1.Y;
+            lin.X2 = line.P2.X;
+            lin.Y2 = line.P2.Y;
+            lin.StrokeThickness = 4;          
+            lin.Stroke = Brushes.Black;
+          
+
+
+            // Circle
+            Ellipse circl = new Ellipse();
+            circl.Width = circle.Diameter;
+            circl.Height = circle.Diameter;
+            Canvas.SetTop(circl, circle.P1.Y);
+            Canvas.SetLeft(circl, circle.P1.X);
+            circl.Fill = Brushes.WhiteSmoke;
+            circl.Stroke = Brushes.DarkRed;
+
+
+            // Ellipse
+            Ellipse el = new Ellipse();
+            el.Width = elipse.Width;
+            el.Height = elipse.Height;
+            Canvas.SetTop(el, elipse.P1.Y);
+            Canvas.SetLeft(el, elipse.P1.X);
+            el.Fill = Brushes.Green;
+            el.Stroke = Brushes.Yellow;
+
+            // Square
+            System.Windows.Shapes.Rectangle squar = new System.Windows.Shapes.Rectangle();
+            squar.Height = square.P1.Y - square.P2.Y;
+            squar.Width = square.P3.X - square.P2.X;
+            Canvas.SetTop(squar, square.P1.Y);
+            Canvas.SetLeft(squar, square.P1.X);
+            squar.Fill = Brushes.Aquamarine;
+            squar.Stroke = Brushes.BurlyWood;
+
+            // Rectangular
+            System.Windows.Shapes.Rectangle rect = new System.Windows.Shapes.Rectangle();
+            rect.Height = rectangle.P1.Y - rectangle.P2.Y;
+            rect.Width = rectangle.P3.X - rectangle.P2.X;
+            Canvas.SetTop(rect, rectangle.P1.Y);
+            Canvas.SetLeft(rect, rectangle.P1.X);
+            rect.Fill = Brushes.Chocolate;
+            rect.Stroke = Brushes.Coral;
+
+
+
+            // Triangle(representis through polygon(cause learn C# only few hours))
+            Polygon trian = new Polygon();
+            trian.Fill = Brushes.Crimson;
+            trian.Stroke = Brushes.DarkCyan;
+
+            System.Windows.Point Point1 = new System.Windows.Point(triangle.P1.X, triangle.P1.Y);
+            System.Windows.Point Point2 = new System.Windows.Point(triangle.P2.X, triangle.P2.Y);
+            System.Windows.Point Point3 = new System.Windows.Point(triangle.P3.X, triangle.P3.Y);
+
+            PointCollection trianPoints = new PointCollection();
+
+            trianPoints.Add(Point1);
+            trianPoints.Add(Point2);
+            trianPoints.Add(Point3);
+            
+            trian.Points = trianPoints;
+
+
+            // add shapes to canvas
+            canv.Children.Add(lin);
+            canv.Children.Add(circl);
+            canv.Children.Add(el);
+            canv.Children.Add(squar);
+            canv.Children.Add(rect);
+            canv.Children.Add(trian);
+
+
+            this.Content = canv;
+
         }
     }
 }
